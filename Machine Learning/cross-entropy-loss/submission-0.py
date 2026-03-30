aff702078537914d37b196a879f2412944f2b78e
ShubhamKanitkar32 <66040104+ShubhamKanitkar32@users.noreply.github.com>
2026-03-29 18:00:25 -0700

    Add: cross-entropy-loss - submission-0

---
+++ Machine Learning/cross-entropy-loss/submission-0.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        term_1 = y_true * np.log(y_pred)
+        term_2 = np.subtract(1, y_true) * np.log(np.subtract(1, y_pred))
+        total = np.sum(term_1 + term_2)
+        return round((-1) / len(y_true) * total, 4)
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
+        # y_pred: predicted probabilities (shape: n_samples x n_classes)
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        term = y_true * np.log(y_pred)
+        total = np.sum(np.sum(term))
+        return round((-1) / len(y_true) * total, 4)
